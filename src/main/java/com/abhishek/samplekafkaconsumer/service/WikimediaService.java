45f853db1b31ed29ba0decefffa6e5250bc972f2
abhishek <mail.abhishekjoshi@gmail.com>
2025-03-24 19:41:34 +0000

    Commit

--- src/main/java/com/abhishek/samplekafkaconsumer/service/WikimediaService.java
+++ src/main/java/com/abhishek/samplekafkaconsumer/service/WikimediaService.java
@@ -2,20 +2,25 @@ package com.abhishek.samplekafkaconsumer.service;
 
 import com.abhishek.samplekafkaconsumer.model.Wikimedia;
 import com.abhishek.samplekafkaconsumer.repository.WikimediaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
 public class WikimediaService {
 
+    private static final Logger logger = LoggerFactory.getLogger(WikimediaService.class);
     private final WikimediaRepository wikimediaRepository;
 
     public WikimediaService(WikimediaRepository wikimediaRepository) {
         this.wikimediaRepository = wikimediaRepository;
     }
 
-    public Mono<Wikimedia> save(Wikimedia wikimedia) {
-        return wikimediaRepository.save(wikimedia);
+    public void save(Wikimedia wikimedia) {
+        wikimediaRepository.save(wikimedia)
+                .subscribe(saved -> logger.info("Document saved: {}", saved),
+                        error -> logger.error("Error while saving: {}", String.valueOf(error)));
     }
 
 }
