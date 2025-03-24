5edc0eda389df463ed668c56422adbff41432a8d
abhishek <mail.abhishekjoshi@gmail.com>
2025-03-24 09:23:46 +0000

    Commit

--- src/main/java/com/abhishek/samplekafkaconsumer/listener/MessageListener.java
+++ src/main/java/com/abhishek/samplekafkaconsumer/listener/MessageListener.java
@@ -1,16 +1,20 @@
 package com.abhishek.samplekafkaconsumer.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageListener {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageListener.class);
+
     @KafkaListener(
             topics = "#{ '${app.kafka.topic}' }",
             groupId = "#{ '${spring.kafka.consumer.group-id}' }"
     )
     public void listen(String message) {
-        System.out.println("Received message: " + message);
+       log.info(message);
     }
 }
